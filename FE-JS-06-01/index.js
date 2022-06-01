e628eb542854aa9d4d86fc5142e6864f16148de8
Joanna Świderska <joannaswiderska@MacBook-Pro-Joanna.local>
2022-06-01 18:55:22 +0200

    Search po API

---
+++ FE-JS-06-01/index.js
@@ -0,0 +1,24 @@
+const getRandomKittyBtn = document.querySelector("#searchKitty");
+
+const addRandomPicture = () => {
+  const url = "https://api.thecatapi.com/v1/images/search";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => generatePicture(data[0].url))
+    // .then((data) => {
+    //   return document.body.insertAdjacentHTML(
+    //     "beforeend",
+    //     `<img data-test = "img-kitty" src ="${url}">${data[0].url}`
+    //   );
+    // })
+    .catch((err) => console.error(err));
+};
+
+const generatePicture = (url) => {
+  const picture = document.createElement("img");
+  picture.setAttribute("data-test", "img-kitty");
+  picture.src = url;
+  document.body.appendChild(picture);
+};
+
+getRandomKittyBtn.addEventListener("click", addRandomPicture);
